b0aa78ac699a18809033e6e70a310e9f64e53c86
thomas.lechaptois <thomas.lechaptois@thomsonreuters.com>
2017-11-24 16:09:00 +0100

    Remove useless var

--- atom/common/options_switches.cc
+++ atom/common/options_switches.cc
@@ -29,9 +29,6 @@ const char kFullScreenable[] = "fullscreenable";
 const char kClosable[]       = "closable";
 const char kFullscreen[]     = "fullscreen";
 
-// Eikon hack Disable chromium sandbox.
-const char kDisableSandbox[] = "disable-sandbox";
-
 // Whether the window should show in taskbar.
 const char kSkipTaskbar[] = "skipTaskbar";
 
